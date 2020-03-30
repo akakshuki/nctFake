88193481ed76d37ed0027fbae751103c8901368b
akakshuki <giangbaccai1207@gmail.com>
2020-03-30 23:19:58 +0700

    add Layout Admin

--- AppAdmin/Controllers/HomeController.cs
+++ AppAdmin/Controllers/HomeController.cs
@@ -13,18 +13,10 @@ namespace AppAdmin.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public PartialViewResult NavigateMenu()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
