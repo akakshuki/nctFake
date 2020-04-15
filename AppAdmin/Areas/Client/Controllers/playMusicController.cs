ef55274f26442ee15a7e3b9c7dea99fb07e240d6
thanhan868192 <thanhan.lan@gmail.com>
2020-04-16 00:21:59 +0700

    update view chua xong

--- AppAdmin/Areas/Client/Controllers/playMusicController.cs
+++ AppAdmin/Areas/Client/Controllers/playMusicController.cs
@@ -1,4 +1,5 @@
 ﻿using AppAdmin.Models.Service;
+using ModelViews.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,19 @@ namespace AppAdmin.Areas.Client.Controllers
         // GET: Client/playMusic
         public ActionResult Index(int id)
         {
+            ViewBag.GetSinger = ApiService.GetAllMusic();
             ViewBag.getMusicById = ApiService.GetMusicById(id);
             ViewBag.getLyrics = ApiService.GetLyricByIdMusic(id);
             return View();
+        }
+        public ActionResult UpdateMusicView(MusicDTO music)
+        {
+            var data = ApiService.UpdateMusicView(music);
+            if (data!= null)
+            {
+                return RedirectToAction("Index", new { id = music.ID });
+            }
+            return RedirectToAction("Index",new { id = music.ID });
         }
     }
 }
--- AppAdmin/Areas/Client/Models/ServiceClient/ApiServiceClient.cs
+++ AppAdmin/Areas/Client/Models/ServiceClient/ApiServiceClient.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using ModelViews.DTOs;
 
 
 namespace AppAdmin.Areas.Client.Models.ServiceClient
@@ -98,7 +97,7 @@ namespace AppAdmin.Areas.Client.Models.ServiceClient
                 Console.WriteLine(e);
                 return false;
             }
-        
+        }
     }
    
 }
